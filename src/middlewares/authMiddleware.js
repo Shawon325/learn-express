fd8ceb6ad847558722db810a93adfddb58b08256
MD Shahidul Bhuiyan ( Shawon ) <mdshahidul3244@gmail.com>
2025-04-24 21:28:32 +0600

    auth middleware log write

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,24 +1,29 @@
 import jwt from 'jsonwebtoken';
 import { error } from '../helpers/apiResponse';
+import logger from '../services/logger/loggerService';
 import {
   HTTP_INTERNAL_SERVER_ERROR,
   HTTP_UNAUTHORIZED,
 } from '../constants/statusCode';
 
 const authMiddleware = (request, response, next) => {
-  const token = request.headers['x-access-token'];
-
-  if (!token) {
-    return response.status(HTTP_UNAUTHORIZED).json(error('Unauthorized'));
-  }
-
   try {
+    const token = request.headers['x-access-token'];
+
+    if (!token) {
+      return response.status(HTTP_UNAUTHORIZED).json(error('Unauthorized'));
+    }
+
     request.user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    logger.info('Token verified');
+
+    return next();
   } catch (exception) {
+    logger.error(`auth-middleware : ${exception.message} `);
+
     return response.status(HTTP_INTERNAL_SERVER_ERROR).json(error(exception.message));
   }
-
-  return next();
-}
+};
 
 export default authMiddleware;
